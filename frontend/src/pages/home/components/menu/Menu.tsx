650de5bb7fda6946700ebed62f2237de558f102d
davidmedev222 <davidmedev@gmail.com>
2023-04-04 15:54:08 -0300

    feat(web): add interface of the home page menu

---
+++ frontend/src/pages/home/components/menu/Menu.tsx
@@ -0,0 +1,37 @@
+import { MenuNav, MenuUser } from '@/pages/home/components'
+import styled, { css } from 'styled-components'
+
+const MenuStyled = styled('aside')<{ show: boolean }>`
+  height: 100%;
+  position: fixed;
+  top: 0;
+  left: -100%;
+  z-index: 1;
+  background-color: #C9DBDB;
+  padding: clamp(1.25rem, 1.25vw, 2.5rem) clamp(1rem, 1vw, 2rem);
+  transition: left 400ms;
+  overflow: auto;
+
+  ::-webkit-scrollbar{
+    display: none;
+  }
+
+  ${({ show }) => show && css`
+    left: 0;
+  `}
+`
+
+interface Props {
+  show: boolean
+}
+
+const Menu = ({ show }: Props): JSX.Element => {
+  return (
+    <MenuStyled show={show}>
+      <MenuUser />
+      <MenuNav />
+    </MenuStyled>
+  )
+}
+
+export { Menu }
