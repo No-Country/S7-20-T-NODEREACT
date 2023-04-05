1636810f86ee6323fb7de1544662733686db98a7
davidmedev222 <davidmedev@gmail.com>
2023-04-05 13:00:03 -0300

    refactor(styles): change styles for the responsive desing of the chatbox home page

--- frontend/src/pages/chatbox/pages/home/ChatboxHomePage.tsx
+++ frontend/src/pages/chatbox/pages/home/ChatboxHomePage.tsx
@@ -1,39 +1,25 @@
-import { Chat, Nav, WidgetNewMessage } from '@/pages/chatbox/pages/home/components'
+import { Chats, Header, Nav, WidgetNewMessage } from '@/pages/chatbox/pages/home/components'
 import styled from 'styled-components'
 
-const ListOfChat = styled('section')`
-  display: flex;
-  flex-direction: column;
-  gap: clamp(.5rem, .5vw, 1rem);
-  padding: clamp(.5rem, .5vw, 1rem) clamp(1rem, 1vw, 2rem);
+const ChatboxHomePageStyled = styled('div')`
+  display: grid;
+  grid-template:
+  "header" auto
+  "tabs" auto
+  "chats" 1fr
+  / 1fr;
+  max-height: 100vh;
+  position: relative;
 `
 
 const ChatboxHomePage = (): JSX.Element => {
   return (
-    <div>
-      <div>
-        <Nav />
-        <ListOfChat>
-          <Chat />
-          <Chat />
-          <Chat />
-          <Chat />
-          <Chat />
-          <Chat />
-          <Chat />
-          <Chat />
-          <Chat />
-          <Chat />
-          <Chat />
-          <Chat />
-          <Chat />
-          <Chat />
-          <Chat />
-          <Chat />
-          <WidgetNewMessage />
-        </ListOfChat>
-      </div>
-    </div>
+    <ChatboxHomePageStyled>
+      <Header />
+      <Nav />
+      <Chats />
+      <WidgetNewMessage />
+    </ChatboxHomePageStyled>
   )
 }
 
