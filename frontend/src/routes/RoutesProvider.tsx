12607122ff355649d11d2c9d1009d2b286ba5fe2
davidmedev222 <davidmedev@gmail.com>
2023-04-04 21:43:13 -0300

    refactor(routes): add new structure of the routes

--- frontend/src/routes/RoutesProvider.tsx
+++ frontend/src/routes/RoutesProvider.tsx
@@ -1,18 +1,22 @@
-import { ChatPage, ErrorPage, HomePage, LayoutPages, LoginPage, NewMessagePage, RegisterPage } from '@/pages'
+import { ErrorPage, LoginPage, RegisterPage } from '@/pages'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 const RoutesProvider = (): JSX.Element => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<LayoutPages />}>
-          <Route index element={<HomePage />} />
-          <Route path='chat' element={<ChatPage />} />
-          <Route path='new-message' element={<NewMessagePage />} />
-          <Route path='login' element={<LoginPage />} />
-          <Route path='register' element={<RegisterPage />} />
+        <Route path='/' element={<main>pages home layout</main>}>
+          <Route index element={<main>home page</main>} />
+          <Route path='features' element={<main>feature page</main>} />
+          <Route path='faqs' element={<main>faqs page</main>} />
+          <Route path='privacy' element={<main>privacy page</main>} />
           <Route path='*' element={<ErrorPage />} />
         </Route>
+        <Route path='/chatbox' element={<main>chatbox page</main>} />
+        <Route path='/chatbox/chat/:chat' element={<main>chatbox page</main>} />
+        <Route path='/chatbox/new-message' element={<main>chatbox new message page</main>} />
+        <Route path='/register' element={<RegisterPage />} />
+        <Route path='/login' element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   )
