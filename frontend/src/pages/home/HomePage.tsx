4a1e67123dc59f0849facbdd19721e3ca0f4cef9
davidmedev222 <davidmedev@gmail.com>
2023-04-07 18:23:37 -0300

    feat(web): add responsive interface of the call to action component to home page

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -1,10 +1,11 @@
-import { Features, Hero } from '@/pages/home/components'
+import { CallToAction, Features, Hero } from '@/pages/home/components'
 
 const HomePage = (): JSX.Element => {
   return (
     <main>
       <Hero />
       <Features />
+      <CallToAction />
     </main>
   )
 }
