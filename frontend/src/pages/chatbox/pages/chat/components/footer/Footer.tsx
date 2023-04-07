7c6dd3553b06518f6c02a2812c41fff04cd2fa66
fernandobouchet <fernandobouchet@gmail.com>
2023-04-07 13:59:56 -0300

    feat(web): add interface of the chat page footer attach options

--- frontend/src/pages/chatbox/pages/chat/components/footer/Footer.tsx
+++ frontend/src/pages/chatbox/pages/chat/components/footer/Footer.tsx
@@ -1,9 +1,12 @@
 import { IconAddReaction, IconAttachFileAdd } from '@/pages/chatbox/pages/chat/components'
+import { useState } from 'react'
 import styled from 'styled-components'
+import { FooterAttachOptions } from './FooterAttachOptions'
 
 const StyledFooter = styled.footer`
   position: sticky;
   bottom:0;
+  width: 100%;
 
   @media screen and (min-width: 49.75rem) {
     padding: 0 1rem;
@@ -12,27 +15,17 @@ const StyledFooter = styled.footer`
   }
 `
 
-const WrapperFooterItems = styled.div`
+const WrapperFooterItems = styled.div<{ show: boolean }>`
   width: 100%;
   margin: auto;
-  height:74px;
+  height: ${props => props.show ? '204px' : '74px'};
+  transition: height 0.4s;
+  overflow: hidden;
   border-top: 2px solid #6361B3;
   display: flex;
-  justify-content: space-between;
-  align-items: center;
+  flex-direction: column;
   border-bottom: clamp(0.125rem, 0.125vw, 0.25rem) solid #281828;
   background-color: #1C111C;
-  padding: clamp(1rem, 1vw, 2rem);
-  
-  svg {
-    width: clamp(2rem, 2vw, 4rem) ;
-    height: clamp(2rem, 2vw, 4rem) ;
-    background: #281828;
-    border-radius: 4px;
-    fill: #9C959C;
-    outline: clamp(0.125rem, 0.125vw, 0.25rem) solid #534653;
-    padding: 0.1rem;
-  }
 
   @media screen and (min-width: 49.75rem) {
     max-width: 40rem;
@@ -57,13 +50,46 @@ const StyledInput = styled.input`
 }
 `
 
+const WrapperMainOptions = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  padding: clamp(1rem, 1vw, 2rem);
+
+  svg {
+    width: clamp(2rem, 2vw, 4rem) ;
+    height: clamp(2rem, 2vw, 4rem) ;
+    background: #281828;
+    border-radius: 4px;
+    fill: #9C959C;
+    outline: clamp(0.125rem, 0.125vw, 0.25rem) solid #534653;
+    padding: 0.1rem;
+  }
+`
+
+const IconActionWrapper = styled.div`
+  cursor: pointer;
+`
+
 const Footer = (): JSX.Element => {
+  const [showAttachOptions, setShowAttachOptions] = useState(false)
+
+  const handleAttachClick = (): void => {
+    setShowAttachOptions(!showAttachOptions)
+  }
+
   return (
     <StyledFooter>
-      <WrapperFooterItems>
-        <IconAddReaction />
-        <StyledInput type='text' placeholder='Enviar mensaje a Dan' />
-        <IconAttachFileAdd />
+      <WrapperFooterItems show={showAttachOptions}>
+        <WrapperMainOptions>
+          <IconAddReaction />
+          <StyledInput type='text' placeholder='Enviar mensaje a Dan' />
+          <IconActionWrapper onClick={handleAttachClick}>
+            <IconAttachFileAdd />
+          </IconActionWrapper>
+        </WrapperMainOptions>
+        <FooterAttachOptions show={showAttachOptions} />
       </WrapperFooterItems>
     </StyledFooter>
   )
