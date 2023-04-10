c0b93f933524110acaf583c4c8ed2f09d0df2def
davidmedev222 <davidmedev@gmail.com>
2023-04-10 18:00:54 -0300

    chore(web): add icons for the menu on the home page header

--- frontend/src/components/icons/IconMenu.tsx
+++ frontend/src/components/icons/IconMenu.tsx
@@ -1,7 +1,11 @@
-const IconMenu = (): JSX.Element => {
+interface Props {
+  boxShadow?: boolean
+}
+
+const IconMenu = ({ boxShadow = true }: Props): JSX.Element => {
   return (
-    <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 448 512'>
-      <path d='M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z' />
+    <svg data-svg-bx={boxShadow} xmlns='http://www.w3.org/2000/svg' viewBox='0 96 960 960'>
+      <path d='M120 816v-60h720v60H120Zm0-210v-60h720v60H120Zm0-210v-60h720v60H120Z' />
     </svg>
   )
 }
